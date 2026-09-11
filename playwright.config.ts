f1ad2bc5e3c40976265d0f72959959472118fb7b
kayser963 <142847175+kayser963@users.noreply.github.com>
2026-09-10 21:14:06 -0400

    fix: creer les snapshots linux

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  updateSnapshots: process.env.CI ? 'all' : 'missing',
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
